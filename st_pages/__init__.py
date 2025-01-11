8c500337008d602beeb86dd99f9af58e645ef16d
entremotivator <104661532+entremotivator@users.noreply.github.com>
2025-01-11 15:20:55 -0500

    Update __init__.py

--- st_pages/__init__.py
+++ st_pages/__init__.py
@@ -1,5 +1,3 @@
-# Importing the necessary functions from each page module
-
 from st_pages.home import run as home
 from st_pages.model_management import run as model_management
 from st_pages.ai_chatbot import run as ai_chatbot
